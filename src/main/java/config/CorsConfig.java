e55fda6c7c5cfb46842980ff97f12c30dc3de006
Ruslan Rybachok <ruslanrybachok@Noutbuk-Ruslan.local>
2023-12-15 20:56:52 +0200

    added cors config

---
+++ src/main/java/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from any origin
+        config.addAllowedOrigin("*");
+
+        // Allow specific HTTP methods
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        // Allow specific headers
+        config.addAllowedHeader("Content-Type");
+        config.addAllowedHeader("Authorization");
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
